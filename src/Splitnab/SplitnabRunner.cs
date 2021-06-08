068065e9a7c1a107a9256766e63a5a16b9988957
Chris Ko <chriskopher@gmail.com>
2021-06-08 12:27:04 -0400

    refactor: split up run method into smaller private methods

--- src/Splitnab/SplitnabRunner.cs
+++ src/Splitnab/SplitnabRunner.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using SplitwiseClient;
 using SplitwiseClient.Model.Expenses;
+using SplitwiseClient.Model.Friends;
+using SplitwiseClient.Model.Users;
 using YnabClient;
+using YnabClient.Model.Accounts;
+using YnabClient.Model.Budgets;
 using YnabClient.Model.Transactions;
 
 namespace Splitnab
@@ -25,82 +30,41 @@ namespace Splitnab
 
         public async Task<Transactions?> Run(AppSettings settings, bool dryRun)
         {
+            // Setup authentication for clients
             await _splitwiseClient.ConfigureAccessToken(settings.Splitwise.ConsumerKey,
                 settings.Splitwise.ConsumerSecret);
             _ynabClient.ConfigureAuthorization(settings.Ynab.PersonalAccessToken);
 
-            // 1. Get desired Splitwise expenses
-            var currentUser = await _splitwiseClient.GetCurrentUser();
-            if (currentUser.User == null)
+            var (swSuccess, (swUser, swFriend, swExpenses)) = await CanGetSplitwiseInfo(settings);
+            if (!swSuccess)
             {
-                _logger.LogWarning("Unable to fetch the current Splitwise user. Exiting...");
+                _logger.LogError("Unable to fetch required Splitwise information");
+                _logger.LogInformation("Verify that the Splitwise section in appsettings.json is configured correctly");
                 return null;
             }
 
-            var friends = await _splitwiseClient.GetFriends();
-            if (friends.Friends == null)
+            var (ynabSuccess, (ynabBudget, ynabAccount)) = await CanGetYnabInfo(settings);
+            if (!ynabSuccess)
             {
-                _logger.LogWarning("Unable to fetch current user's Splitwise friends list. Exiting...");
+                _logger.LogError("Unable to fetch required YNAB information");
+                _logger.LogInformation("Verify that the YNAB section in appsettings.json is configured correctly");
                 return null;
             }
 
-            var friend = friends.Friends.FirstOrDefault(x => x.Email == settings.Splitwise.FriendEmail);
-            if (friend == null)
+            // Map expenses as YNAB transactions
+            var ynabTransactions = swExpenses.Select(expense => new SaveTransaction
             {
-                _logger.LogWarning("Unable to find the specified Splitwise friend. Exiting...");
-                return null;
-            }
-
-            var expenses = await _splitwiseClient.GetExpenses(
-                friendId: friend.Id,
-                datedAfter: settings.Splitwise.TransactionsDatedAfter,
-                limit: 0);
-            if (expenses.Expenses == null)
-            {
-                _logger.LogWarning("No Splitwise expenses found for the specified user and date range. Exiting...");
-                return null;
-            }
-
-            // 2. Map them as YNAB transactions
-            var ynabBudgets = await _ynabClient.GetBudgets(true);
-            if (ynabBudgets.Data?.Budgets == null)
-            {
-                _logger.LogWarning("Unable to fetch YNAB budgets. Exiting...");
-                return null;
-            }
-
-            var ynabBudget = ynabBudgets.Data.Budgets.FirstOrDefault(x => x.Name == settings.Ynab.BudgetName);
-            if (ynabBudget == null)
-            {
-                _logger.LogWarning("Unable to find YNAB budget. Exiting...");
-                return null;
-            }
-
-            var ynabBudgetAccounts = await _ynabClient.GetBudgetAccounts(ynabBudget.Id);
-            var splitwiseAccount = ynabBudgetAccounts.Data?.Accounts?
-                .SingleOrDefault(x => x.Name == settings.Ynab.SplitwiseAccountName);
-            if (splitwiseAccount == null)
-            {
-                _logger.LogWarning(
-                    "Unable to find YNAB splitwise account {SplitwiseAccountName} in budget {YnabBudgetName}. Exiting...",
-                    settings.Ynab.SplitwiseAccountName, settings.Ynab.BudgetName);
-                return null;
-            }
-
-            var ynabTransactions = expenses.Expenses.Select(expense => new SaveTransaction
-            {
-                AccountId = splitwiseAccount.Id,
+                AccountId = ynabAccount.Id,
                 Date = expense.Date.GetValueOrDefault(),
-                Amount = CalculateYnabAmount(expense, currentUser.User.Id),
+                Amount = CalculateYnabAmount(expense, swUser.Id),
                 PayeeName = string.Join(" ",
-                    new[] {friend.FirstName, friend.LastName}.Where(x => !string.IsNullOrWhiteSpace(x))),
+                    new[] {swFriend.FirstName, swFriend.LastName}.Where(x => !string.IsNullOrWhiteSpace(x))),
                 Memo = expense.Description,
                 Approved = false
             });
 
-            // 3. Save them as YNAB transactions
+            // Save YNAB transactions
             var transactionsToPost = new Transactions {SaveTransactions = ynabTransactions.ToList()};
-
             if (dryRun)
             {
                 foreach (var transaction in transactionsToPost.SaveTransactions)
@@ -121,6 +85,76 @@ namespace Splitnab
             }
 
             return transactionsToPost;
+        }
+
+        private async Task<(bool success, (User, FriendModel, List<Expense>) info)> CanGetSplitwiseInfo(
+            AppSettings settings)
+        {
+            // Validate that required Splitwise information is retrievable
+            var currentUser = await _splitwiseClient.GetCurrentUser();
+            if (currentUser.User == null)
+            {
+                _logger.LogWarning("Unable to fetch the current Splitwise user");
+                return (false, (new User(), new FriendModel(), new List<Expense>()));
+            }
+
+            var friends = await _splitwiseClient.GetFriends();
+            if (friends.Friends == null)
+            {
+                _logger.LogWarning("Unable to fetch current user's Splitwise friends list");
+                return (false, (currentUser.User, new FriendModel(), new List<Expense>()));
+            }
+
+            var friend = friends.Friends?.FirstOrDefault(x => x.Email == settings.Splitwise.FriendEmail);
+            if (friend == null)
+            {
+                _logger.LogWarning("Unable to find the specified Splitwise friend");
+                return (false, (currentUser.User, new FriendModel(), new List<Expense>()));
+            }
+
+            // Get desired Splitwise expenses
+            var expenses = await _splitwiseClient.GetExpenses(
+                friendId: friend.Id,
+                datedAfter: settings.Splitwise.TransactionsDatedAfter,
+                limit: 0);
+            if (expenses.Expenses == null)
+            {
+                _logger.LogWarning("No Splitwise expenses found for the specified user and date range");
+                return (false, (currentUser.User, friend, new List<Expense>()));
+            }
+
+            return (true, (currentUser.User, friend, expenses.Expenses));
+        }
+
+        private async Task<(bool success, (BudgetSummary, Account) info)> CanGetYnabInfo(AppSettings settings)
+        {
+            // Validate that required YNAB information is retrievable
+            var ynabBudgets = await _ynabClient.GetBudgets(true);
+            if (ynabBudgets.Data?.Budgets == null)
+            {
+                _logger.LogWarning("Unable to fetch YNAB budgets");
+                return (false, (new BudgetSummary(), new Account()));
+            }
+
+            var ynabBudget = ynabBudgets.Data.Budgets.FirstOrDefault(x => x.Name == settings.Ynab.BudgetName);
+            if (ynabBudget == null)
+            {
+                _logger.LogWarning("Unable to find YNAB budget");
+                return (false, (new BudgetSummary(), new Account()));
+            }
+
+            var ynabBudgetAccounts = await _ynabClient.GetBudgetAccounts(ynabBudget.Id);
+            var splitwiseAccount =
+                ynabBudgetAccounts.Data?.Accounts?.SingleOrDefault(x => x.Name == settings.Ynab.SplitwiseAccountName);
+            if (splitwiseAccount == null)
+            {
+                _logger.LogWarning(
+                    "Unable to find YNAB splitwise account {SplitwiseAccountName} in budget {YnabBudgetName}",
+                    settings.Ynab.SplitwiseAccountName, settings.Ynab.BudgetName);
+                return (false, (ynabBudget, new Account()));
+            }
+
+            return (true, (ynabBudget, splitwiseAccount));
         }
 
         private long CalculateYnabAmount(Expense expense, int currentUserId)
